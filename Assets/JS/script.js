9e228076834371a3c64015d56292664e9881b91a
Aster Oliva <asterthealabaster@gmail.com>
2024-04-11 01:35:55 -0700

    rebuilt filepaths, added todos to script.js

---
+++ Assets/JS/script.js
@@ -0,0 +1,7 @@
+// TODO: Searchbar (search for city, change forecast to that city, & add city to search history)
+
+// TODO: Search History (change display back to clicked city in history)
+
+// TODO: Current Weather (name, date, weather icon, temp, humidity, & wind speed)
+
+// TODO: 5-Day Forecast (date, weather icon, temp, humidity, & wind speed for each day card)
